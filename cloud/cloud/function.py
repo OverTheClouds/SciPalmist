27ea308bcbc8d358896136d8adf4b92e8fb82089
lihao <lihao3016@gmail.com>
2018-02-19 23:55:14 +0800

    ok

--- cloud/cloud/function.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response, render
-def post(request):
-    ctx = {}
-    if request.POST:
-        ctx['rlt'] = request.POST['q']
-    return render(request, "post.html", ctx)
-    
